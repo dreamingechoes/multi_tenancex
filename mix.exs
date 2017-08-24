79d2deb5cccca2be9e7624d36e2e48bab82e6ed0
Ivan Gonzalez <hi@dreamingecho.es>
2017-08-24 15:30:53 +0200

    Update mix.exs and mix.lock

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule MultiTenancex.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      # Default Phoenix stuff
       {:phoenix, "~> 1.3.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
