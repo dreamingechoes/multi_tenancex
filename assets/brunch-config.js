0fee9c6aa4d855800e55863e754a942727a1b2e0
Ivan Gonzalez <hi@dreamingecho.es>
2017-08-24 15:30:05 +0200

    Add sass config and nom global variables in brunch-config.js

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -3,21 +3,6 @@ exports.config = {
   files: {
     javascripts: {
       joinTo: "js/app.js"
-
-      // To use a separate vendor.js bundle, specify two files path
-      // http://brunch.io/docs/config#-files-
-      // joinTo: {
-      //   "js/app.js": /^js/,
-      //   "js/vendor.js": /^(?!js)/
-      // }
-      //
-      // To change the order of concatenation of files, explicitly mention here
-      // order: {
-      //   before: [
-      //     "vendor/js/jquery-2.1.1.js",
-      //     "vendor/js/bootstrap.min.js"
-      //   ]
-      // }
     },
     stylesheets: {
       joinTo: "css/app.css"
@@ -47,7 +32,13 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/vendor/]
-    }
+    },
+    sass: {
+      mode: 'native',
+      options: {
+        includePaths: ['node_modules']
+      }
+    },
   },
 
   modules: {
@@ -57,6 +48,11 @@ exports.config = {
   },
 
   npm: {
-    enabled: true
+    enabled: true,
+    globals: {
+      "jQuery": "jquery",
+      "$": "jquery",
+      "Popper": "popper.js"
+    }
   }
 };
