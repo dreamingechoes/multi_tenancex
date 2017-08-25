e97ccf9743c070077dbf91dfac15f6031a19254f
Ivan Gonzalez <hi@dreamingecho.es>
2017-08-25 18:40:42 +0200

    Add guardian configuration in config.exs

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,16 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+# Guardian configuration
+config :guardian, Guardian,
+  allowed_algos: ["HS512"],
+  verify_module: Guardian.JWT,
+  issuer: "MultiTenancex",
+  ttl: { 30, :days },
+  allowed_drift: 2000,
+  verify_issuer: true,
+  serializer: MultiTenancexWeb.GuardianSerializer
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
