2fa3107ef58bfefb45e1fc65f421bff0675a5f3c
Ivan Gonzalez <hi@dreamingecho.es>
2017-08-25 18:39:29 +0200

    Add new routes in router.ex

--- lib/multi_tenancex_web/router.ex
+++ lib/multi_tenancex_web/router.ex
@@ -13,14 +13,59 @@ defmodule MultiTenancexWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :admin_layout do
+    plug :put_layout, {MultiTenancexWeb.Admin.LayoutView, :admin}
+  end
+
+  pipeline :browser_session do
+    plug Guardian.Plug.VerifySession
+    plug Guardian.Plug.LoadResource
+  end
+
+  pipeline :authenticated_admin do
+    plug Guardian.Plug.EnsureAuthenticated, handler: MultiTenancexWeb.Admin.SessionController
+    plug Guardian.Plug.EnsureResource, handler: MultiTenancexWeb.Admin.SessionController
+
+    # Custom plugs
+    plug MultiTenancexWeb.Plug.CurrentAdmin
+    plug MultiTenancexWeb.Plug.CurrentAdminTenant
+  end
+
+  pipeline :unauthenticated_admin do
+    plug Guardian.Plug.EnsureNotAuthenticated, handler: MultiTenancexWeb.Admin.SessionController
+  end
+
   scope "/", MultiTenancexWeb do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :browser
 
     get "/", PageController, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", MultiTenancexWeb do
-  #   pipe_through :api
-  # end
+  # Admin scope
+  scope "/admin", MultiTenancexWeb.Admin, as: :admin do
+    pipe_through [:browser, :browser_session]
+
+    # Admin unauthenticated scope
+    scope "/" do
+      pipe_through [:unauthenticated_admin]
+
+      resources "/session", SessionController, only: [:new, :create]
+    end
+
+    # Admin authenticated scope
+    scope "/" do
+      pipe_through [:admin_layout, :authenticated_admin]
+
+      # Log out resource
+      get "/logout", SessionController, :delete
+
+      # Switch tenant resource
+      post "/switch_tenant", DashboardController, :switch_tenant
+
+      resources "/", DashboardController, only: [:index]
+      resources "/administrators", AdministratorController
+      resources "/companies", CompanyController
+      resources "/products", ProductController
+    end
+  end
 end
