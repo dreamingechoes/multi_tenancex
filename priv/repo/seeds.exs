800fcfbcfe372af4540be3bdb473e490079d8a2d
Ivan Gonzalez <hi@dreamingecho.es>
2018-06-03 23:34:44 +0200

    Add some seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,34 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     MultiTenancex.Repo.insert!(%MultiTenancex.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias MultiTenancex.Accounts
+alias MultiTenancex.Companies
+
+# Create a default administrator
+Accounts.create_administrator(%{
+  email: "example@example.com",
+  password: "123456",
+  firstname: "Example",
+  lastname: "Example"
+})
+
+# Create four companies with some products
+Enum.each(0..3, fn _ ->
+  {:ok, company} =
+    Companies.create_company(%{
+      name: Faker.Company.name(),
+      description: Faker.Lorem.paragraph(%Range{first: 1, last: 2}),
+      image: Faker.Avatar.image_url(800, 200)
+    })
+
+  Enum.each(0..6, fn n ->
+    Companies.create_product(
+      %{
+        name: Faker.Lorem.word(),
+        description: Faker.Lorem.paragraph(%Range{first: 1, last: 2}),
+        image: Faker.Avatar.image_url(200, 120),
+        company_id: company.id,
+        price: 29.95,
+        units: 10 * (n + 1)
+      },
+      "tenant_" <> company.slug
+    )
+  end)
+end)
