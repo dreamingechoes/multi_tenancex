458274183001dbfd81dd693e01807a57c6447980
Ivan Gonzalez <hi@dreamingecho.es>
2018-06-03 23:31:48 +0200

    Add generate_slug method to company schema

--- lib/multi_tenancex/companies/company.ex
+++ lib/multi_tenancex/companies/company.ex
@@ -21,7 +21,23 @@ defmodule MultiTenancex.Companies.Company do
   def changeset(%Company{} = company, attrs) do
     company
     |> cast(attrs, [:name, :description, :image, :slug])
-    |> validate_required([:name, :description, :image, :slug])
+    |> validate_required([:name, :description, :image])
     |> unique_constraint(:name)
+    |> generate_slug()
+  end
+
+  defp generate_slug(current_changeset) do
+    case current_changeset do
+      %Ecto.Changeset{valid?: true, changes: %{name: name}} ->
+        slug =
+          name
+          |> String.downcase()
+          |> String.replace(" ", "_")
+
+        put_change(current_changeset, :slug, slug)
+
+      _ ->
+        current_changeset
+    end
   end
 end
