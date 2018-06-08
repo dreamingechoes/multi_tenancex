07ffb25cf749a4fefd3a18195535727d434a6dca
dreamingechoes <hi@dreamingecho.es>
2018-06-08 19:38:57 +0200

    Add company creation in test helper for performance

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,14 @@
+ExUnit.configure(exclude: [skip: true])
 ExUnit.start()
 
+# Create a tenant for tests. For performance reasons the tenant is created and
+# migrated only once before running all the tests.
+{:ok, _company} =
+  MultiTenancex.Companies.create_company(%{
+    description: "some description",
+    image: "some image",
+    name: "some name"
+  })
+
 Ecto.Adapters.SQL.Sandbox.mode(MultiTenancex.Repo, :manual)
+Code.compiler_options(ignore_module_conflict: true)
